1715825c4fee5d9c56ca955208bb7ed0a69e11bf
Gideon VanRiette <gvanriette@gmail.com>
2013-12-08 16:20:52 +0900

    Added constructor functions to llist.c; implemented createList(), append(), and prepend(). Added prototypes for destructor functions.

--- llist/llist.c
+++ llist/llist.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <assert.h>
 
+/* A basic singly-linked list implementation for integers. To make many 
+ * operations easier, each list has a first element that contains metadata 
+ * about the list itself. */ 
+
 typedef struct node {
   struct node* next;
   int data;
@@ -10,51 +14,96 @@ typedef struct node {
 typedef struct llist {
   node* head;
   node* tail;
+  int length; 
 } llist;
 
-llist* createList(int);
-void appendEnd(llist*, int);
-void printList(llist*); 
+/* The createNode() and createEmptyList() are basic constructor functions that are 
+ * used in the remainder of the functions here. */ 
 
-int main(int argc, char *argv[])
+node* createNode(int number, node* nextNode)
 {
-  llist *myList = createList(100);
-  for (int i = 0; i < 20; ++i)
-  {
-    appendEnd(myList, i);
-  }
-  
-  printList(myList);
+  node* newNode = (node *) malloc(sizeof(node));
+  assert(newNode != NULL);
 
-  return 0;
+  newNode->data = number;
+  newNode->next = nextNode;
+
+  return newNode;
 }
 
-llist* createList(int number) 
+llist* createEmptyList() 
 {
   llist *newList = malloc(sizeof(llist));
   assert(newList != NULL);
 
-  node* elem = (node *) malloc(sizeof(node));
-  assert(elem != NULL);
+  newList->head = NULL;
+  newList->tail = NULL;
+  newList->length = 0;
 
-  elem->data = number;
-  elem->next = NULL;
-
-  newList->head = elem;
-  newList->tail = elem;
   return newList;
 }
 
-void appendEnd(llist* list, int number)
+/* The destroyNode() and destroyList() are destructor methods that make sure 
+ * the memory that is allocated to a list is freed properly. */ 
+
+void destroyNode(node* node)
 {
-  node* elem = (node *) malloc(sizeof(node));
-  assert(elem != NULL);
 
-  elem->data = number;
-  elem->next = NULL;
+}
 
-  list->tail->next = elem;
-  list->tail = elem;
+void destroyList(llist* list)
+{
+
+}
+
+llist* createList(int number) 
+{
+  llist* newList = createEmptyList();
+  node* newNode = createNode(number, NULL);
+  
+  newList->head = newNode;
+  newList->tail = newNode;
+  newList->length = 1;
+
+  return newList;
+}
+
+/* append() and prepend() insert a new node with the value 'number' into the 
+ * linked list 'list' */ 
+
+void append(llist* list, int number)
+{
+  node* newNode = createNode(number, NULL);
+
+  if (list->length == 0) 
+  {
+    list->head = newNode;
+  } else {
+    list->tail->next = newNode;
+  }
+
+  list->tail = newNode;
+  list->length += 1;
+}
+
+void prepend(llist* list, int number)
+{
+  node* newNode = createNode(number, NULL);
+
+  if (list->length == 0)
+  {
+    list->tail = newNode;
+  }
+
+  newNode->next = list->head;
+  list->head = newNode;
+  list->length += 1;
+}
+
+node* pop(llist* list)
+{
+
+
 }
 
 void printList(llist* list) 
