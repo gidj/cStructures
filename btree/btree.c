e9adf81cb06ea0ffdebc52b04e76254466e87179
Gideon VanRiette <gvanriette@gmail.com>
2013-12-30 22:51:50 +0900

    Reworked btree to be generic, using pointers

--- btree/btree.c
+++ btree/btree.c
@@ -1,273 +1,206 @@
-/* A simple binary tree data structure for integers. Any node of the tree
- * can be acted upon like another tree itself. Lower integers are stored on
- * the left, and higher integers are stored on the right. */ 
-
 #include "btree.h"
+#include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 
-struct node {
-    int key;
-    void* value;
-    struct node *left;
-    struct node *right;
+struct Node {
+  int key;
+  void* value;
+  int elementSize;
+  Node left, right;
 };
 
-/* Constructor function create_tree and destructor function destroy_tree 
- * abstract away the details of how we create and destroy the tree. */ 
-
-node create_tree(int key, 
-                 void* value,
-                 node left,
-                 node right)
+Node tree_create(int key, void* value, int elementSize, Node left, Node right)
 {
-  node new_node = (node) malloc(sizeof(node));
-  assert(new_node);
+  assert(value);
+  assert(elementSize > 0);
+  
+  Node node = malloc(sizeof(*node));
+  node->key = key;
+  node->value = malloc(elementSize);
+  memcpy(node->value, value, elementSize);
+  node->left = left;
+  node->right = right;
 
-  key_set(new_node, key);
-  value_set(new_node, value);
-  left_set(new_node, left);
-  right_set(new_node, right);
-
-  return new_node;
+  return node;
 }
 
-void destroy_tree(node tree)
-{
-  if (tree)
-  {
-    destroy_tree(left(tree));
-    destroy_tree(right(tree));
-
-    free(tree);
-  }
-}
-
-/* Selector functions. */ 
-
-int key(node tree)
-{
-  return tree->key;
-}
-
-node left(node tree)
-{
-  return tree->left;
-}
-
-node right(node tree)
-{
-  return tree->right;
-}
-
-/* Functions to set the values of the tree variables*/ 
-
-void key_set(node tree, int key)
-{
-  tree->key = key;
-}
-
-void value_set(node tree, void* value)
-{
-  assert(tree && value);
-  if (sizeof(tree->value) != sizeof(*value))
-  {
-    tree->value = realloc(tree->value, sizeof(*value));
-  }
-  memcpy(tree->value, value, sizeof(*value));
-}
-
-void left_set(node tree, node child)
-{
-  tree->left = child;
-}
-
-void right_set(node tree, node child)
-{
-  tree->right = child;
-}
-
-/* min() and max() functions to return a pointer to the node with the minimum 
- * and maximum values, respectively. */ 
-
-node min(node tree)
-{
-  if (tree == NULL)
-  {
-     /* ToDo: currently this function returns a NULL pointer if the tree is empty;
-      * later, create an error handling routine to return an error instead to signal
-      * that there are no elementes in the tree. */ 
-    return tree;
-  } else if (left(tree) == NULL) {
-    return tree;
-  } else {
-    return min(left(tree));
-  }
-}
-
-node max(node tree)
+void tree_destroy(Node tree)
 {
   assert(tree);
+  Node left = tree->left;
+  Node right = tree-> right;
 
-  if (right(tree) == NULL) {
-    return tree;
-  } else {
-    return max(right(tree));
-  }
+  free(tree->value);
+  free(tree);
+
+  if (left) { tree_destroy(left); }
+  if (right) { tree_destroy(right); }
 }
 
-/* Printing functions */ 
-
-void print_preorder(node tree)
+void* min_value(Node tree)
 {
-  if (tree)
-  {
-    printf("%d\n", value(tree));
-    print_preorder(left(tree));
-    print_preorder(right(tree));
+  assert(tree);
+  Node current = tree;
+  
+  while(current->left) {
+    current = current->left;
   }
+  return current->value;
 }
 
-void print_inorder(node tree)
+void* max_value(Node tree)
 {
-  if (tree)
-  {
-    print_inorder(left(tree));
-    printf("%d\n", data(tree));
-    print_inorder(right(tree));
+  assert(tree);
+  Node current = tree;
+  
+  while(current->right) {
+    current = current->right;
   }
+  return current->value;
 }
 
-void print_postorder(node tree)
+void* tree_search(Node tree, int key)
 {
-  if (tree)
-  {
-    print_postorder(left(tree));
-    print_postorder(right(tree));
-    printf("%d\n", data(tree));
-  }
+  assert(tree);
+  Node current = tree;
+
+  do {
+    if      (key == current->key) { return current->value; }
+    else if (key < current->key) { current = current->left; } 
+    else if (key > current->key) { current = current->right; }
+  } while(current); 
+
+  return NULL;
 }
 
-/* Search function that returns the node containing the given object of 
- * interest, or NULL if it cannot be found. */ 
-
-node search_tree(node tree, int query)
+void tree_insert(Node tree, int key, void* value, int elementSize)
 {
-  if (!tree)
-  {
-    return NULL;
-  }  
+  assert(tree);
+  Node current = tree;
 
-  if (query == key(tree))
+  if (key == current->key)
   {
-    return tree;
+    current->value = realloc(current->value, elementSize);
+    memcpy(current->value, value, elementSize);
+    current->elementSize = elementSize;
+    return;
   } 
-  else if (query < data(tree)) 
-  {
-    return search_tree(left(tree), query);
-  } 
-  else 
-  {
-    return search_tree(right(tree), query);
-  }
-}
-
-/* insert() takes a pointer to a pointer that points to a tree, and a value to
- * insert into that tree. It recursively checks to see if the value exists; if 
- * so, the function exits and nothing is done. If not and it reaches a NULL 
- * leaf, a new node is created with the provided value and it is inserted in
- * place of the NULL node. */ 
-
-void insert(node *tree, int key, void* value)
-{
-  if ((*tree) == NULL)
-  {
-    (*tree) = create_tree(key, value, NULL, NULL);
-  }
-  else if ((*tree)->key > key)
-  {
-    tree = &((*tree)->left);
-    insert(tree, key, value);
-  } 
-  else if ((*tree)->key < key) 
-  {
-    tree = &((*tree)->right);
-    insert(tree, key, value);
-  }
-  else 
-  {
-/* This is the case where the value at the node equals the value provided to 
- * the function
- * TODO: Add error codes via an integer type. */ 
-  }
-}
-
-/* delete_node() is primarily a helper function to delete_data(). There are three 
- * major cases: (1) the node has no children; (2) the node has only one child; 
- * and (3) the node has two children. There is also the unlikely case that the 
- * pointer is simply NULL. */ 
-
-void delete_node(node* n)
-{
-  if (n == NULL)
-  {
-  }
-  else if (!left(*n) && !right(*n))
-  {
-    node temp = *n;
-    (*n) = NULL;
-    free(temp->value);
-    free(temp);
-  }
-  else if (!left(*n))
-  {
-    node temp = *n;
-    (*n) = right(*n);
-    free(temp->value);
-    free(temp);
-  }
-  else if (!right(*n))
-  {
-    node temp = *n;
-    (*n) = left(*n);
-    free(temp->value);
-    free(temp);
-  } else
-  {
-
-/* The final case, where there are two children to the node to be deleted. 
- * We find the successor, which is simply the node with the minimum key value
- * on the right side of the tree. If that success is ALSO the right child of 
- * tree, then it is a simple case of simply 'moving' the right child to take
- * the place of the head of tree. If the successor is in the right subtree but
- * is NOT the right child, the data from successor is moved into the head of 
- * tree, and then successor's own right child takes its place. This is done
- * via a recursive calle to the delete_node() function, as it nicely fits one
- * of the above cases. */ 
-
-    node successor = min(right(*n));
-
-    if (successor == right(*n))
+  else if (key < current->key)
+  { 
+    if (current->left)
     {
-      key_set(*n, key(successor));
-      value_set(*n, value(successor));
-      right_set(*n, right(successor));
-      free(successor->value);
-      free(successor);
+      tree_insert(current->left, key, value, elementSize);
+    } 
+    else
+    {
+      current->left = tree_create(key, value, elementSize, NULL, NULL);
+      return;
+    }
+  } 
+  else if (key > current->key)
+  {
+    if (current->right)
+    {
+      tree_insert(current->right, key, value, elementSize);
     }
     else
     {
-      key_set(*n, key(successor));
-      value_set(*n, value(successor));
-      delete_node(&successor);
+      current->right = tree_create(key, value, elementSize, NULL, NULL);
+      return;
     }
   }
 }
 
-/* */ 
-
-void delete_value(node tree, int key)
+void tree_delete(Node tree, int key)
 {
-  node element = search_tree(tree, key);
-  delete_node(&element);
+  if (tree)
+  {
+    if (key == tree->key)
+    {
+      node_delete(&tree);
+    }
+    else if (key < tree->key)
+    {
+      tree_delete(tree->left, key);
+    }
+    else if (key > tree->key)
+    {
+      tree_delete(tree->right, key);
+    }
+  }
+  return;
 }
 
+void node_delete(Node *node)
+{
+  if (*node)
+  {
+    Node current = *node;
+    
+    if (!current->left && !current->right)
+    {
+      tree_destroy(current);
+      *node = NULL;
+      return;
+    }
+    else if (!current->left)
+    {
+      *node = current->right;
+      free(current->value);
+      free(current);
+      return;
+    }
+    else if (!current->right)
+    {
+      *node = current->left;
+      free(current->value);
+      free(current);
+      return;
+    }
+    else
+    {
+      Node successor = min_node(current->right);
+
+    }
+      
+  }
+
+}
+
+Node node_left(Node node)
+{
+  return node->left;
+}
+
+Node node_right(Node node)
+{
+  return node->right;
+}
+
+Node min_node(Node tree)
+{
+  assert(tree);
+  Node current = tree;
+  
+  while(current->left) {
+    current = current->left;
+  }
+  return current;
+}
+
+Node max_node(Node tree)
+{
+  assert(tree);
+  Node current = tree;
+  
+  while(current->right) {
+    current = current->right;
+  }
+  return current;
+}
+
+void* node_data(Node node)
+{
+  return node->value;
+}
--- btree/btree.h
+++ btree/btree.h
@@ -1,38 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-
 #ifndef BTREE_H_
-#define BTREE_H_
+#define BTREE_H_ 
 
-typedef struct node *node;
+typedef struct Node *Node;
 
-extern node create_tree(int key,
-                        void* value,
-                        node left, 
-                        node right);
-extern void  destroy_tree(node tree);
+extern Node tree_create(int key, void* value, int elementSize, Node left, Node right);
+extern void tree_destroy(Node tree); 
 
-extern int key(node n);
-extern void* value(node tree);
-extern node left(node tree);
-extern node right(node tree);
+extern void* min_value(Node tree);
+extern void* max_value(Node tree);
+extern void* tree_search(Node tree, int key);
 
-extern void key_set(node tree, int key);
-extern void value_set(node tree, void* value);
-extern void left_set(node tree, node child);
-extern void right_set(node tree, node child);
+extern void tree_insert(Node tree, int key, void* value, int elementSize);
+extern void tree_delete(Node tree, int key); 
 
-extern node min(node tree);
-extern node max(node tree);
-
-extern void print_preorder(node tree);
-extern void print_inorder(node tree);
-extern void print_postorder(node tree);
-
-extern node search_tree(node tree, int value);
-extern void insert(node* tree, int key, void* value);
-extern void node_delete(node* tree);
-extern void data_delete(node tree, int key);
+extern Node node_left(Node node);
+extern Node node_right(Node node);
+extern Node min_node(Node tree);
+extern Node max_node(Node tree);
+extern void node_delete(Node *node);
 
 #endif
