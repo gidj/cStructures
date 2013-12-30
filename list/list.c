3f1fd2f0aedc0873d11b0a81b830632aa38670c7
Gideon VanRiette <gvanriette@gmail.com>
2013-12-30 09:39:03 +0900

    Removed llist, and replaced it with list; list is generic, using pointers and memcpy to hold any data type

---
+++ list/list.c
@@ -0,0 +1,125 @@
+#include "list.h"
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+struct List {
+  int length;
+  int elementSize;
+  Node head;
+  Node tail;
+};
+
+struct Node {
+  void *data;
+  Node next;
+};
+
+List list_new(int elementSize)
+{
+  assert(elementSize > 0);
+  List new_list = malloc(sizeof(*new_list));
+
+  new_list->length = 0;
+  new_list->elementSize = elementSize;
+  new_list->head = new_list->tail = NULL;
+
+  return new_list;
+}
+
+void list_destroy(List list)
+{
+  assert(list);
+  Node current;
+
+  while(list->head) {
+    current = list->head;
+    list->head = current->next;
+    free(current->data);
+    free(current);
+  }
+
+  free(list);
+}
+
+void list_append(List list, void *element)
+{
+  assert(list);
+  assert(element);
+
+  Node new_node = malloc(sizeof(*new_node));
+  new_node->data = malloc(list->elementSize);
+  memcpy(new_node->data, element, list->elementSize);
+
+  new_node->next = NULL;
+
+  if (list->length == 0)
+  {
+    list->head = list->tail = new_node;
+  } 
+  else 
+  {
+    list->tail->next = new_node;
+    list->tail = new_node;
+  }
+    
+  list->length++;
+}
+
+void list_prepend(List list, void *element)
+{
+  assert(list);
+  assert(element);
+
+  Node new_node = malloc(sizeof(*new_node));
+  new_node->data = malloc(list->elementSize);
+  memcpy(new_node->data, element, list->elementSize);
+  
+  new_node->next = list->head;
+  list->head = new_node;
+
+  if (!list->tail)
+  {
+    list->tail = new_node;
+  }
+
+  list->length++;
+}
+
+
+int list_length(List list)
+{
+  return list->length;
+}
+
+void* list_head(List list, void *element, bool removeElement)
+{
+  assert(list->head);
+
+  memcpy(element, list->head->data, list->elementSize);
+
+  if (removeElement)
+  {
+    Node elem = list->head;
+    list->head = list->head->next;
+    free(elem->data);
+    free(elem);
+    list->length--;
+  }
+  
+  return element;
+}
+
+void* list_tail(List list, void *element)
+{
+  assert(list->tail);
+  memcpy(element, list->tail->data, list->elementSize);
+  return element;
+}
+
+void* node_data(Node node)
+{
+  return node->data;
+}
+
+
---
+++ list/list.h
@@ -0,0 +1,20 @@
+#include <stdbool.h>
+
+#ifndef LIST_H_
+#define LIST_H_
+
+typedef struct List *List;
+typedef struct Node *Node;
+
+extern List list_new(int elementSize);
+extern void list_destroy(List list);
+
+extern void list_append(List list, void *element);
+extern void list_prepend(List list, void *element);
+
+extern int list_length(List list);
+extern void* list_head(List list, void *element, bool removeElement);
+extern void* list_tail(List list, void *element);
+extern void* node_data(Node node);
+
+#endif
